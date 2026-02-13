cd4d9e52afa5dd9a839ff8816b1c04178e7bacde
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-02-13 17:32:09 +0600

    update

--- resources/js/pages/frontend/lpa.tsx
+++ resources/js/pages/frontend/lpa.tsx
@@ -4,6 +4,7 @@ import { LpaCtaSection } from '@/components/frontend/lpa/lpa-cta-section';
 import { LpaFaqSection } from '@/components/frontend/lpa/lpa-faq-section';
 import { LpaFeaturedLogosSection } from '@/components/frontend/lpa/lpa-featured-logos-section';
 import { LpaHeroSection } from '@/components/frontend/lpa/lpa-hero-section';
+import { LpaPackageOptionsSection } from '@/components/frontend/lpa/lpa-package-options-section';
 import { LpaPlanningSection } from '@/components/frontend/lpa/lpa-planning-section';
 import { LpaPricingSection } from '@/components/frontend/lpa/lpa-pricing-section';
 import { LpaSupportSection } from '@/components/frontend/lpa/lpa-support-section';
@@ -15,6 +16,7 @@ export default function Lpa() {
         <FrontendLayout>
             <main >
                 <LpaHeroSection />
+                <LpaPackageOptionsSection />
                 <LpaFeaturedLogosSection />
                 <LpaCtaSection />
                 <LpaPlanningSection />
