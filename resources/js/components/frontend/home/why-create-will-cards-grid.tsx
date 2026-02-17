0d5f6894d144bb18c102d6356d0153fba3f35c2c
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-02-17 10:35:00 +0600

    update

--- resources/js/components/frontend/home/why-create-will-cards-grid.tsx
+++ resources/js/components/frontend/home/why-create-will-cards-grid.tsx
@@ -109,6 +109,7 @@ export function WhyCreateWillCardsGrid() {
                         </div>
                     ))}
                 </div>
+                
             </div>
         </section>
     );
