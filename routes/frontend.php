35565a2a84befa889b57834766c4b246ee51f125
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-02-17 09:57:23 +0600

    some changes

--- routes/frontend.php
+++ routes/frontend.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get("/", [FrontendController::class, "index"])->name("home");
-    Route::get("/horizon-wills", [FrontendController::class, "horizonWills"])->name("horizon-wills");
+    Route::get("/will-writing-online", [FrontendController::class, "horizonWills"])->name("horizon-wills");
     Route::get("/contact", [FrontendController::class, "contact"])->name("contact");
     Route::get("/will-writing", [FrontendController::class, "willWriting"])->name("will-writing");
     Route::get("/will-writing/start", [FrontendController::class, "willWritingStart"])->name("will-writing.start");
