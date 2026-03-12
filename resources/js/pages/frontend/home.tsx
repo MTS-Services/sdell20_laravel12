3b701f64a287d31454fe2454794f4fad7c828d0f
Md Shariful Islam <devshariful99@gmail.com>
2026-03-12 16:34:10 +0600

    Add SEO meta tags to frontend pages
    Add Inertia <Head> meta tags (title, description, keywords) to multiple frontend pages to improve SEO and social preview. Import Head from @inertiajs/react and wrap page content in a fragment so metadata can be injected alongside existing layout. Pages updated: home, will-writing, lpa, probate, contact, cookie-policy, privacy-policy, terms-and-conditions, consumer-rights-act, and investment-opportunity. Also add a canonical link and update the default app title in resources/views/app.blade.php.

--- resources/js/pages/frontend/home.tsx
+++ resources/js/pages/frontend/home.tsx
@@ -11,21 +11,31 @@ import { ManagingAffairsSection } from '@/components/frontend/home/managing-affa
 import { WhyCreateWillCardsGrid } from '@/components/frontend/home/why-create-will-cards-grid';
 import { WhyCreateWillSection } from '@/components/frontend/home/why-create-will-section';
 import FrontendLayout from '@/layouts/frontend-layout';
+import { Head } from '@inertiajs/react';
 
 export default function Home() {
     return (
-        <FrontendLayout>
-            <Banner />
-            {/* <CTASection /> */}
-            <WhyCreateWillSection />
-            <WhyCreateWillCardsGrid />
-            <HowItWorksSection />
-            {/* <DashboardPreviewSection /> */}
-            <EstateApproachSection />
-             <FindingSupportSection />
-            <ManagingAffairsSection />
-            <LoveOnesSection />
-           
-        </FrontendLayout>
+        <>
+            <Head>
+                <title>Online Will Writing Service</title>
+                <meta name="title" content="Online Will Writing Service" />
+                <meta name="description" content="Write your will online with our step-by-step tool. Checked by a will specialist. Great for simple wills and estates." />
+                <meta name="keywords" content="will, online will, will writing, estate planning, inheritance, legal documents" />
+            </Head>
+            <FrontendLayout>
+                <Banner />
+                {/* <CTASection /> */}
+                <WhyCreateWillSection />
+                <WhyCreateWillCardsGrid />
+                <HowItWorksSection />
+                {/* <DashboardPreviewSection /> */}
+                <EstateApproachSection />
+                <FindingSupportSection />
+                <ManagingAffairsSection />
+                <LoveOnesSection />
+
+            </FrontendLayout>
+
+        </>
     );
 }
--- resources/js/pages/frontend/probate.tsx
+++ resources/js/pages/frontend/probate.tsx
@@ -6,17 +6,26 @@ import { PreferToTalkSection } from '@/components/frontend/probate/prefer-to-tal
 import { SpecialistSupportSection } from '@/components/frontend/probate/specialist-support-section';
 import { WhatIsProbateSection } from '@/components/frontend/probate/what-is-probate-section';
 import FrontendLayout from '@/layouts/frontend-layout';
+import { Head } from '@inertiajs/react';
 
 export default function Probate() {
     return (
-        <FrontendLayout>
-            <main>
-                <HeroSection />
-                <SpecialistSupportSection />
-                <WhatIsProbateSection />
-                <HowWeHelpSection />
-                <PreferToTalkSection />
-            </main>
-        </FrontendLayout>
+        <>
+            <Head>
+                <title>Probate Fee</title>
+                <meta name="title" content="Probate Fee" />
+                <meta name="description" content="Apply for probate online and get a free quote" />
+                <meta name="keywords" content="Probate application, Probate on line, Get probate online, Solicitor fees probate, probate costs" />
+            </Head>
+            <FrontendLayout>
+                <main>
+                    <HeroSection />
+                    <SpecialistSupportSection />
+                    <WhatIsProbateSection />
+                    <HowWeHelpSection />
+                    <PreferToTalkSection />
+                </main>
+            </FrontendLayout>
+        </>
     );
 }
